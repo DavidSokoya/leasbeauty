726cb7ecb6b1c93aebd1f9ea03a989345f72e844
DavidSokoya <sokoya08@gmail.com>
2025-02-13 06:41:32 +0100

    ref

--- backend/controller/bookingController.js
+++ backend/controller/bookingController.js
@@ -1,6 +1,9 @@
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 import { Booking } from '../model/BookingsSchema.js';
 
-
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
   
  const  getBookings =  async (req, res) => {
     try {
